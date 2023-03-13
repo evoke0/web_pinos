01dd2b85cdd219d8aa70c7c661802483d0720917
evoke0 <ale-jimenez1@hotmail.es>
2023-03-13 12:39:35 +0100

    Añadida alerta informativa para asociarse 	modified:   index.html 	modified:   javascript/scripts.js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,11 @@
 const hamburguesa = document.getElementById("hamburguesa");
 const navBar = document.getElementById("nav-bar");
+const asociarse = document.getElementById("asociarse");
 
 hamburguesa.addEventListener("click", () => {
     navBar.classList.toggle("active");
+})
+
+asociarse.addEventListener("click", () => {
+    alert("Por favor, siga las instrucciones descritas en el ANEXO para rellenar el formulario.\n\nUna vez completado, envíelo mediante correo electrónico a la dirección indicada en el margen superior derecho o entréguelo en la sede.");
 })
