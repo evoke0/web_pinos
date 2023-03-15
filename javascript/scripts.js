8db51863930c49e5d06de315108140e1f025074b
evoke0 <ale-jimenez1@hotmail.es>
2023-03-15 12:24:33 +0100

    Cambiada imagen de los estatutos 	modified:   assets/img/feature-1.png Cambiado colores 	modified:   css/style.css Eliminado temporalmente código innecesario 	modified:   javascript/scripts.js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,10 +1,10 @@
-const hamburguesa = document.getElementById("hamburguesa");
-const navBar = document.getElementById("nav-bar");
+// const hamburguesa = document.getElementById("hamburguesa");
+// const navBar = document.getElementById("nav-bar");
 const asociarse = document.getElementById("asociarse");
 
-hamburguesa.addEventListener("click", () => {
-    navBar.classList.toggle("active");
-})
+// hamburguesa.addEventListener("click", () => {
+//     navBar.classList.toggle("active");
+// })
 
 asociarse.addEventListener("click", () => {
     alert("Por favor, siga las instrucciones descritas en el ANEXO para rellenar el formulario.\n\nUna vez completado, envíelo mediante correo electrónico a la dirección indicada en el margen superior derecho o entréguelo en la sede.");
